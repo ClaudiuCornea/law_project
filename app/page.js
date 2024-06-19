2c60bb7415df4fa6deb4bcfdc3b9549e62d499ed
Claudiu <claudiu.simion.cornea@gmail.com>
2024-06-19 22:43:42 +0200

    start

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full m-0 p-0">
+      {/* Hero */}
+      <div className="h-screen w-full m-0 p-0 flex bg-[url('/skyscrapers.jpg')] bg-cover bg-center brightness-75">
+        
+      </div>
+
+      {/* Section 1 */}
+      <div className="h-screen w-full m-0 p-0 flex bg-blue-900">
+
+      </div>
+    </main>
+  );
+}
